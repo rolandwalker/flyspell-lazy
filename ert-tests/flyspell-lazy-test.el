9ba1d54521ed57a2756de9839303a65aa5611ca2
Roland Walker <walker@pobox.com>
2012-09-21 13:42:30 -0400

    include build and autoload with test target add stub test file no individual tests defined, though

---
+++ ert-tests/flyspell-lazy-test.el
@@ -0,0 +1,20 @@
+
+;; requires and setup
+
+(require 'flyspell-lazy)
+
+;;; this is a stub - no tests defined
+
+;;
+;; Emacs
+;;
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+;;
+
+;;; flyspell-lazy-test.el ends here
