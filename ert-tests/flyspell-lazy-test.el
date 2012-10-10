96431228a8b35ae5c235c5e77a59f1a144171a47
Roland Walker <walker@pobox.com>
2012-10-10 13:28:13 -0400

    comment needed tests

--- ert-tests/flyspell-lazy-test.el
+++ ert-tests/flyspell-lazy-test.el
@@ -5,6 +5,38 @@
 
 ;;; this is a stub - no tests defined
 
+;;; flyspell-lazy-checkable-buffer-p (&optional buffer)
+
+;;; flyspell-lazy-sort-and-merge-spans (nearby)
+
+;;; flyspell-lazy-refine-changes (&optional add-point)
+
+;;; flyspell-lazy-strip-text (text)
+
+;;; flyspell-lazy-matches-last-text ()
+
+;;; flyspell-lazy-hurry (seconds)
+
+;;; flyspell-lazy-user-just-completed-word ()
+
+;;; flyspell-lazy-prev-word-contains-error ()
+
+;;; flyspell-lazy-uncheck-buffer (&optional buffer)
+
+;;; flyspell-lazy-ignored-buffer-p (&optional buffer)
+
+;;; flyspell-lazy-after-change-function (start stop len)
+
+;;; flyspell-lazy-unload (&optional global)
+
+;;; flyspell-lazy-load ()
+
+;;; flyspell-lazy-check-pending ()
+
+;;; flyspell-lazy-check-visible ()
+
+;;; flyspell-lazy-check-buffer (&optional force)
+
 ;;
 ;; Emacs
 ;;
