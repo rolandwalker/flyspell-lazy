8471dbe2984b302cc34a1b93f746890edbb58fb0
Roland Walker <walker@pobox.com>
2013-11-06 10:06:41 -0500

    update/stdize copyrights

--- flyspell-lazy.el
+++ flyspell-lazy.el
@@ -1,6 +1,6 @@
 ;;; flyspell-lazy.el --- Improve flyspell responsiveness using idle timers
 ;;
-;; Copyright (c) 2012 Roland Walker
+;; Copyright (c) 2012-13 Roland Walker
 ;;
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/flyspell-lazy
@@ -142,9 +142,6 @@
 ;;; License
 ;;
 ;;    Simplified BSD License
-;;
-;;    Copyright (c) 2012, Roland Walker
-;;    All rights reserved.
 ;;
 ;;    Redistribution and use in source and binary forms, with or
 ;;    without modification, are permitted provided that the following
