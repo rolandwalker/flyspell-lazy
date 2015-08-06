1a33a6c16b05cb45536b302d7fc3bb7ed6356f67
Roland Walker <walker@pobox.com>
2015-08-06 16:44:32 -0400

    whitespace/format/comments/docstrings

--- flyspell-lazy.el
+++ flyspell-lazy.el
@@ -141,38 +141,38 @@
 ;;
 ;;; License
 ;;
-;;    Simplified BSD License
+;; Simplified BSD License
 ;;
-;;    Redistribution and use in source and binary forms, with or
-;;    without modification, are permitted provided that the following
-;;    conditions are met:
+;; Redistribution and use in source and binary forms, with or
+;; without modification, are permitted provided that the following
+;; conditions are met:
 ;;
-;;       1. Redistributions of source code must retain the above
-;;          copyright notice, this list of conditions and the following
-;;          disclaimer.
+;;   1. Redistributions of source code must retain the above
+;;      copyright notice, this list of conditions and the following
+;;      disclaimer.
 ;;
-;;       2. Redistributions in binary form must reproduce the above
-;;          copyright notice, this list of conditions and the following
-;;          disclaimer in the documentation and/or other materials
-;;          provided with the distribution.
+;;   2. Redistributions in binary form must reproduce the above
+;;      copyright notice, this list of conditions and the following
+;;      disclaimer in the documentation and/or other materials
+;;      provided with the distribution.
 ;;
-;;    This software is provided by Roland Walker "AS IS" and any express
-;;    or implied warranties, including, but not limited to, the implied
-;;    warranties of merchantability and fitness for a particular
-;;    purpose are disclaimed.  In no event shall Roland Walker or
-;;    contributors be liable for any direct, indirect, incidental,
-;;    special, exemplary, or consequential damages (including, but not
-;;    limited to, procurement of substitute goods or services; loss of
-;;    use, data, or profits; or business interruption) however caused
-;;    and on any theory of liability, whether in contract, strict
-;;    liability, or tort (including negligence or otherwise) arising in
-;;    any way out of the use of this software, even if advised of the
-;;    possibility of such damage.
+;; This software is provided by Roland Walker "AS IS" and any express
+;; or implied warranties, including, but not limited to, the implied
+;; warranties of merchantability and fitness for a particular
+;; purpose are disclaimed.  In no event shall Roland Walker or
+;; contributors be liable for any direct, indirect, incidental,
+;; special, exemplary, or consequential damages (including, but not
+;; limited to, procurement of substitute goods or services; loss of
+;; use, data, or profits; or business interruption) however caused
+;; and on any theory of liability, whether in contract, strict
+;; liability, or tort (including negligence or otherwise) arising in
+;; any way out of the use of this software, even if advised of the
+;; possibility of such damage.
 ;;
-;;    The views and conclusions contained in the software and
-;;    documentation are those of the authors and should not be
-;;    interpreted as representing official policies, either expressed
-;;    or implied, of Roland Walker.
+;; The views and conclusions contained in the software and
+;; documentation are those of the authors and should not be
+;; interpreted as representing official policies, either expressed
+;; or implied, of Roland Walker.
 ;;
 ;;; Code:
 ;;
